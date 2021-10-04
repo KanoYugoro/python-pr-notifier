abd9f8e582b594d125e17fb3f3f4c7ac55209918
Anson Wayman <kanoyugoro@gmail.com>
2021-10-04 13:42:54 -0600

    Finish API fetching logic and all associated tests

--- pyprnotifier/github_retrieval.py
+++ pyprnotifier/github_retrieval.py
@@ -2,10 +2,13 @@ import requests
 import datetime
 import re
 
+from requests.api import request
+
 def isPRTooOld(PRobj, cutoffTime):
-    regexGroups = re.search("(?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+)T",PRobj.updated_at)
+    updateTime = PRobj['updated_at']
+    regexGroups = re.search("(?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+)T",updateTime)
     if regexGroups == None:
-        raise ValueError(f'Invalid date pulled from Pull Request update_at field: {PRobj.updated_at}')
+        raise ValueError(f'Invalid date pulled from Pull Request update_at field: {updateTime}')
     PRTime = datetime.date(int(regexGroups.group('year')),int(regexGroups.group('month')),int(regexGroups.group('day')))
     return cutoffTime > PRTime
     
@@ -13,7 +16,31 @@ def getCutoffTime(currentDate, timeDelta):
     return (currentDate - timeDelta)
 
 def get_PRs(org,repo,pr_state = 'all',page_number = 1,req_module = requests):
-    return req_module.get(f'https://api.github.com/repos/{org}/{repo}/pulls',{'state':pr_state,'sort':'updated','page':page_number,'per_page':100})
+    return req_module.get(f'https://api.github.com/repos/{org}/{repo}/pulls',{'state':pr_state,'sort':'updated','page':page_number})
 
 def get_Recent_PRs(currentDate, timeDelta, org, repo, pr_state = 'all', req_module = requests):
-    requestResult = get_PRs(org,repo,pr_state,1,req_module)
+    jsonCollection = []
+    page_number = 0
+    cutoffDate = getCutoffTime(currentDate, timeDelta)
+    foundCutoff = False
+
+    while (not foundCutoff):
+        page_number = page_number + 1
+        requestResult = get_PRs(org,repo,pr_state,page_number,req_module).json()
+        if (requestResult == []): # We've exceeded the number of pages
+            foundCutoff = True
+            break
+
+        index = 0
+        while (index < len(requestResult) and not foundCutoff):
+            foundCutoff = isPRTooOld(requestResult[index], cutoffDate)
+            if (not foundCutoff):
+                index = index + 1
+        
+        if (not foundCutoff or index > len(requestResult)): #So we went all the way through the first page and we haven't found the end of the PRs in the time span we want.
+            jsonCollection = jsonCollection + requestResult
+        elif (foundCutoff): #we found the PR in the list that is too old to care about, so append to that point
+            jsonCollection = jsonCollection + (requestResult[0:index])
+    
+    return jsonCollection
+    
--- tests/test_github_retrieval.py
+++ tests/test_github_retrieval.py
@@ -2,42 +2,77 @@ from pyprnotifier import github_retrieval
 import unittest
 import datetime
 
-class MockModel:
-    pass
+class MockRequestResult:
+    def __init__(self, jsonBlob):
+        self.parsedJsonBlob = jsonBlob
+    def json(self):
+        return self.parsedJsonBlob
 
 class MockGithubRequests:
     def get(self, url, paramsdict):
         if (url == 'https://api.github.com/repos/dummyEmptyOrg/dummyEmptyRepo/pulls'):
-            return []
+            return MockRequestResult(jsonBlob = [])
         elif (url == 'https://api.github.com/repos/dummyOrg/dummyRepo/pulls'):
-            return [{
-                'number': 1,
-                'state': 'open',
-                'title': 'The First PR',
-                'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/1',
-                'updated_at': '2011-01-16T19:01:12Z',
-                'user': {
-                    'login': 'CoolUser13'
-                }
-            },{
-                'number': 2,
-                'state': 'closed',
-                'title': 'The Second PR',
-                'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/2',
-                'updated_at': '2011-01-17T19:01:12Z',
-                'user': {
-                    'login': 'OtherGuy'
-                }
-            },{
-                'number': 3,
-                'state': 'open',
-                'title': 'The Third PR',
-                'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/3',
-                'updated_at': '2011-01-18T19:01:12Z',
-                'user': {
-                    'login': 'CoolUser13'
-                }
-            }]
+            if paramsdict['page'] == 1:
+                return MockRequestResult(jsonBlob = [{
+                    'number': 6,
+                    'state': 'closed',
+                    'title': 'The Sixth PR',
+                    'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/6',
+                    'updated_at': '2011-01-21T19:01:12Z',
+                    'user': {
+                        'login': 'OtherGuy'
+                    }
+                },{
+                    'number': 5,
+                    'state': 'open',
+                    'title': 'The Fifth PR',
+                    'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/5',
+                    'updated_at': '2011-01-20T19:01:12Z',
+                    'user': {
+                        'login': 'CoolUser13'
+                    }
+                },{
+                    'number': 4,
+                    'state': 'closed',
+                    'title': 'The Fourth PR',
+                    'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/4',
+                    'updated_at': '2011-01-19T19:01:12Z',
+                    'user': {
+                        'login': 'OtherGuy'
+                    }
+                }])
+            elif paramsdict['page'] == 2:
+                return MockRequestResult(jsonBlob = [{
+                    'number': 3,
+                    'state': 'open',
+                    'title': 'The Third PR',
+                    'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/3',
+                    'updated_at': '2011-01-18T19:01:12Z',
+                    'user': {
+                        'login': 'CoolUser13'
+                    }
+                },{
+                    'number': 2,
+                    'state': 'open',
+                    'title': 'The Second PR',
+                    'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/2',
+                    'updated_at': '2011-01-17T19:01:12Z',
+                    'user': {
+                        'login': 'OtherGuy'
+                    }
+                },{
+                    'number': 1,
+                    'state': 'closed',
+                    'title': 'The First PR',
+                    'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/1',
+                    'updated_at': '2011-01-16T19:01:12Z',
+                    'user': {
+                        'login': 'CoolUser13'
+                    }
+                }])
+            elif paramsdict['page'] == 3: #Tested github api, if you specify a page past what exists it returns an empty set.
+                return MockRequestResult(jsonBlob = [])
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
@@ -46,18 +81,15 @@ class BasicTestSuite(unittest.TestCase):
         assert True
 
     def test_isPRTooOld_yes(self):
-        PRObject = MockModel()
-        PRObject.updated_at = '2011-01-26T19:01:12Z'
+        PRObject = {'updated_at': '2011-01-26T19:01:12Z'}
         self.assertTrue(github_retrieval.isPRTooOld(PRObject,datetime.date(2012,1,1)))
     
     def test_isPRTooOld_no(self):
-        PRObject = MockModel()
-        PRObject.updated_at = '2011-01-26T19:01:12Z'
+        PRObject = {'updated_at': '2011-01-26T19:01:12Z'}
         self.assertFalse(github_retrieval.isPRTooOld(PRObject,datetime.date(2011,1,25)))
     
     def test_isPRTooOld_invalidInput(self):
-        PRObject = MockModel()
-        PRObject.updated_at = 'NotADate'
+        PRObject = {'updated_at': 'NotADate'}
         self.assertRaisesRegex(ValueError, 'Invalid date pulled from Pull Request update_at field: NotADate', github_retrieval.isPRTooOld, PRObject, datetime.date(2011,1,25))
 
     def test_getCutoffTime(self):
@@ -67,12 +99,49 @@ class BasicTestSuite(unittest.TestCase):
     
     def test_get_PRs(self):
         ghq = MockGithubRequests()
-        self.assertEqual(github_retrieval.get_PRs('dummyEmptyOrg','dummyEmptyRepo',req_module=ghq),[])
+        self.assertEqual(github_retrieval.get_PRs('dummyEmptyOrg','dummyEmptyRepo',req_module=ghq).json(),[])
     
     def test_get_PRs_withcontent(self):
         ghq = MockGithubRequests()
-        self.assertEqual(len(github_retrieval.get_PRs('dummyOrg','dummyRepo',req_module=ghq)),3)
+        self.assertEqual(len(github_retrieval.get_PRs('dummyOrg','dummyRepo',page_number=1, req_module=ghq).json()),3)
 
+    def test_get_Recent_PRs_ResultOnFirstPage(self):
+        ghq = MockGithubRequests()
+        #This test should grab only the first two PRs in the list, because the "current date" is set for 7 days ahead of the 5th PR
+        result = github_retrieval.get_Recent_PRs(datetime.date(2011,1,27), datetime.timedelta(days=7), 'dummyOrg', 'dummyRepo', pr_state = 'all', req_module = ghq)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0]['number'],6)
+        self.assertEqual(result[0]['title'],'The Sixth PR')
+        self.assertEqual(result[1]['number'],5)
+        self.assertEqual(result[1]['title'],'The Fifth PR')
+    
+    def test_get_Recent_PRs_ResultOnSecondPage(self):
+        ghq = MockGithubRequests()
+        #This test should grab only the first two PRs in the list, because the "current date" is set for 7 days ahead of the 5th PR
+        result = github_retrieval.get_Recent_PRs(datetime.date(2011,1,24), datetime.timedelta(days=7), 'dummyOrg', 'dummyRepo', pr_state = 'all', req_module = ghq)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0]['number'],6)
+        self.assertEqual(result[0]['title'],'The Sixth PR')
+        self.assertEqual(result[1]['number'],5)
+        self.assertEqual(result[1]['title'],'The Fifth PR')
+        self.assertEqual(result[2]['number'],4)
+        self.assertEqual(result[2]['title'],'The Fourth PR')
+        self.assertEqual(result[3]['number'],3)
+        self.assertEqual(result[3]['title'],'The Third PR')
+        self.assertEqual(result[4]['number'],2)
+        self.assertEqual(result[4]['title'],'The Second PR')
+
+    def test_get_Recent_PRs_AllResultsMatter(self):
+        ghq = MockGithubRequests()
+        #This test should grab all PRs in the list, they should fall within 7 days of this date.
+        result = github_retrieval.get_Recent_PRs(datetime.date(2011,1,22), datetime.timedelta(days=7), 'dummyOrg', 'dummyRepo', pr_state = 'all', req_module = ghq)
+        self.assertEqual(len(result), 6)
+
+    def test_get_Recent_PRs_NoResultsMatter(self):
+        ghq = MockGithubRequests()
+        #This test should grab no PRs in the list, because it is more than 7 days past the last date.
+        result = github_retrieval.get_Recent_PRs(datetime.date(2011,1,29), datetime.timedelta(days=7), 'dummyOrg', 'dummyRepo', pr_state = 'all', req_module = ghq)
+        self.assertEqual(len(result), 0)
 
 if __name__ == '__main__':
     unittest.main()
