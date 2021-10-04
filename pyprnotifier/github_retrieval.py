20132b36f095e6a2596b6f75894671b0c40ca446
Anson Wayman <kanoyugoro@gmail.com>
2021-10-04 12:46:56 -0600

    Add some logic around the API and appropriate testing for that logic.

--- pyprnotifier/github_retrieval.py
+++ pyprnotifier/github_retrieval.py
@@ -9,8 +9,11 @@ def isPRTooOld(PRobj, cutoffTime):
     PRTime = datetime.date(int(regexGroups.group('year')),int(regexGroups.group('month')),int(regexGroups.group('day')))
     return cutoffTime > PRTime
     
+def getCutoffTime(currentDate, timeDelta):
+    return (currentDate - timeDelta)
 
-def get_PRs(org,repo,pr_state = 'all',req_module = requests):
-    r = req_module.get(f'https://api.github.com/repos/{org}/{repo}/pulls',{'state':pr_state,'sort':'updated'})
-    print(r.headers)
-    return r.json()
+def get_PRs(org,repo,pr_state = 'all',page_number = 1,req_module = requests):
+    return req_module.get(f'https://api.github.com/repos/{org}/{repo}/pulls',{'state':pr_state,'sort':'updated','page':page_number,'per_page':100})
+
+def get_Recent_PRs(currentDate, timeDelta, org, repo, pr_state = 'all', req_module = requests):
+    requestResult = get_PRs(org,repo,pr_state,1,req_module)
--- tests/test_github_retrieval.py
+++ tests/test_github_retrieval.py
@@ -5,6 +5,40 @@ import datetime
 class MockModel:
     pass
 
+class MockGithubRequests:
+    def get(self, url, paramsdict):
+        if (url == 'https://api.github.com/repos/dummyEmptyOrg/dummyEmptyRepo/pulls'):
+            return []
+        elif (url == 'https://api.github.com/repos/dummyOrg/dummyRepo/pulls'):
+            return [{
+                'number': 1,
+                'state': 'open',
+                'title': 'The First PR',
+                'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/1',
+                'updated_at': '2011-01-16T19:01:12Z',
+                'user': {
+                    'login': 'CoolUser13'
+                }
+            },{
+                'number': 2,
+                'state': 'closed',
+                'title': 'The Second PR',
+                'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/2',
+                'updated_at': '2011-01-17T19:01:12Z',
+                'user': {
+                    'login': 'OtherGuy'
+                }
+            },{
+                'number': 3,
+                'state': 'open',
+                'title': 'The Third PR',
+                'html_url': 'https://github.com/dummyOrg/dummyRepo/pull/3',
+                'updated_at': '2011-01-18T19:01:12Z',
+                'user': {
+                    'login': 'CoolUser13'
+                }
+            }]
+
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
@@ -25,6 +59,19 @@ class BasicTestSuite(unittest.TestCase):
         PRObject = MockModel()
         PRObject.updated_at = 'NotADate'
         self.assertRaisesRegex(ValueError, 'Invalid date pulled from Pull Request update_at field: NotADate', github_retrieval.isPRTooOld, PRObject, datetime.date(2011,1,25))
+
+    def test_getCutoffTime(self):
+        now = datetime.date(2011,1,25)
+        a_week_ago = github_retrieval.getCutoffTime(now, datetime.timedelta(days=7))
+        self.assertEqual(a_week_ago, datetime.date(2011,1,18))
+    
+    def test_get_PRs(self):
+        ghq = MockGithubRequests()
+        self.assertEqual(github_retrieval.get_PRs('dummyEmptyOrg','dummyEmptyRepo',req_module=ghq),[])
+    
+    def test_get_PRs_withcontent(self):
+        ghq = MockGithubRequests()
+        self.assertEqual(len(github_retrieval.get_PRs('dummyOrg','dummyRepo',req_module=ghq)),3)
 
 
 if __name__ == '__main__':
